54dca8bffa41c59ba3b44dbce9fbe47b0fb2673c
AMAN SINGH <amansingh635217@gmail.com>
2026-06-29 09:17:41 +0530

    POTD

---
+++ Number of Strings That Appear as Substrings in Word.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    public:
+        int numOfStrings(vector<string>& patterns, string word) {
+            int ans = 0;
+            for(auto it:patterns){
+                if(word.find(it) != string::npos) ans++;
+            }
+            return ans;
+        }
+    };
